140ef1a23f507941bc619f8ba35a70d433c529fd
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-04-19 16:18:09 +0900

    [chore] 삼성 코테 후 정리(코테합 1주차 내용 병합)

---
+++ 프로그래머스/3/43162. 네트워크/loc_네트워크.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	freopen_s(new FILE*, "input.txt", "r", stdin);
+
+	int tCase; cin >> tCase;
+	while (tCase--) {
+		int n; cin >> n;
+
+		// 입력 받기
+		vector<vector<int>> computers(n, vector<int>(n));
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cin >> computers[i][j];
+			}
+		}
+
+		int result = solution(n, computers);
+		cout << result << "\n";
+	}
+	
+	return 0;
+}
---
+++ 프로그래머스/3/92343. 양과 늑대/loc_양과늑대.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+// 여러 테스트
+void init() {
+    for (int i = 0; i < MX; i++) {
+        v[i].clear();
+        visited[i] = 0;
+    }
+    mxSheep = 0;
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+    // 5
+    cout << solution({ 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1 },
+        { {0, 1}, {1, 2}, {1, 4}, {0, 8}, {8, 7}, {9, 10}, {9, 11}, {4, 3}, {6, 5}, {4, 6}, {8, 9} }
+    ) << endl;
+
+    init();
+
+    // 5
+    cout << solution({ 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0 },
+        { {0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6}, {3, 7}, {4, 8}, {6, 9}, {9, 10} }
+    ) << endl; 
+
+	return 0;
+}

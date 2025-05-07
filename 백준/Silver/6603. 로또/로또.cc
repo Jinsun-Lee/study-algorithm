36e0266037a7de23a11ab99abe1182ad33ba26c6
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-05-07 17:56:48 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.cc
+++ 백준/Silver/6603. 로또/로또.cc
@@ -1,22 +1,21 @@
 #include <iostream>
-#include <vector>
-#define endl "\n"
 using namespace std;
 
-int K;       // K개 수(7 ~ 12 개)를 골라 집합을 만들어
-int num[14]; // 집합 S
-int ans[6];  // 1~49에서 6개
+int K, num[14];
+int ans[6];
 
-void backTrack(int depth, int start) {
-	if (depth >= 6) { // K보다 클 때가 아니라 무조건 6개
-		for (int i = 0; i < 6; i++) cout << ans[i] << " ";
-		cout << endl;
+void dfs(int depth, int start) {
+	if (depth >= 6) { 
+		for (int i = 0; i < 6; ++i){
+            cout << ans[i] << " ";
+        }
+		cout << "\n";
 		return;
 	}
 
-	for (int i = start; i < K; i++) { // K는 포함하지 않아야 해
+	for (int i = start; i < K; ++i) {
 		ans[depth] = num[i];
-		backTrack(depth + 1, i + 1);
+		dfs(depth + 1, i+1);
 	}
 }
 
@@ -24,18 +23,13 @@ int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	//freopen_s(new FILE*, "input.txt", "r", stdin);
 
-	while (1) {
-		cin >> K;
+	while (cin >> K) {
 		if (K == 0) break;
 		else {
-
-			// 입력 받아
-			for (int i = 0; i < K; i++) cin >> num[i];
-			backTrack(0, 0);
-
-			cout << endl; // 케이스 사이에 빈줄
+			for (int i = 0; i < K; ++i) cin >> num[i];
+			dfs(0, 0);
+			cout << "\n";
 		}
 	}
-
 	return 0;
 }

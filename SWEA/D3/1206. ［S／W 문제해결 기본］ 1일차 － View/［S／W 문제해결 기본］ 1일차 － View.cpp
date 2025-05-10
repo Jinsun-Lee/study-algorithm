8738951bb7053febe9545442f15cf9e8f6e7783d
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-05-10 22:48:16 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 5 ms, Memory: 400 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.cpp
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.cpp
@@ -5,21 +5,21 @@ using namespace std;
 int arr[1005];
 
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0);
+    ios::sync_with_stdio(0); cin.tie(0);
 
-	int N, cnt, diff;
-	for (int t = 1; t <= 10; ++t) {
-		
-		cin >> N;
-		cnt = 0;
-		for (int i = 0; i < N; ++i) cin >> arr[i];
+    int N, cnt, diff;
+    for (int t = 1; t <= 10; ++t) {
 
-		for (int i = 2; i < N-2; ++i) {
-			diff = max({arr[i-2], arr[i-1], arr[i+1], arr[i+2]});
-			if (arr[i] > diff) cnt += arr[i] - diff;
-		}
+        cin >> N;
+        cnt = 0;
+        for (int i = 0; i < N; ++i) cin >> arr[i];
 
-		cout << "#" << t << " " << cnt << "\n";
-	}
-	return 0;
+        for (int i = 2; i < N - 2; ++i) {
+            diff = max({ arr[i - 2], arr[i - 1], arr[i + 1], arr[i + 2] });
+            if (arr[i] > diff) cnt += arr[i] - diff;
+        }
+
+        cout << "#" << t << " " << cnt << "\n";
+    }
+    return 0;
 }

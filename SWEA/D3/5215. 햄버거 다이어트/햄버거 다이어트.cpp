b4dae7905fad38628ef925006a0be669bd93e15a
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-05-10 22:41:56 +0900

    [D3] Title: 햄버거 다이어트, Time: 115 ms, Memory: 5,988 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.cpp
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
+ 
 int N, L, rate[20], cal[20], maxAns;
 
-// 칼로리 기준으로 만들고, 맛이 가장 좋은 걸로 출력
 void dfs(int depth, int rSum, int cSum) {
-	if (cSum > L) return;
-	maxAns = max(maxAns, rSum);
-	if (depth >= N) return;
-
-	// 현재 음식 포함/미포함
-	dfs(depth + 1, rSum + rate[depth], cSum + cal[depth]);
-	dfs(depth + 1, rSum, cSum);
+    if (cSum > L) return;
+    maxAns = max(maxAns, rSum);
+    if (depth >= N) return;
+ 
+    // 현재 음식 포함/미포함
+    dfs(depth + 1, rSum + rate[depth], cSum + cal[depth]);
+    dfs(depth + 1, rSum, cSum);
 }
-
+ 
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0);
-
-	int tCase; cin >> tCase;
-	for (int t = 1; t <= tCase; ++t) {
-		cin >> N >> L;
-		for (int i = 0; i < N; ++i) {
-			cin >> rate[i] >> cal[i];
-		}
-		maxAns = 0;
-		dfs(0, 0, 0);
-		cout << "#" << t << " " << maxAns << "\n";
-	}
-	return 0;
+    ios::sync_with_stdio(0); cin.tie(0);
+ 
+    int tCase; cin >> tCase;
+    for (int t = 1; t <= tCase; ++t) {
+        cin >> N >> L;
+        for (int i = 0; i < N; ++i) {
+            cin >> rate[i] >> cal[i];
+        }
+        maxAns = 0;
+        dfs(0, 0, 0);
+        cout << "#" << t << " " << maxAns << "\n";
+    }
+    return 0;
 }

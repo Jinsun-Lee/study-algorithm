71f842db696414c43a2491d9a3c6a4743fe49318
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-05-17 13:20:01 +0900

    [Gold III] Title: 감시, Time: 4 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,83 +1,94 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue> 
+#include <vector>
+#include <cstring>
 using namespace std;
-#define X first
-#define Y second
 
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1}; // 남쪽, 동쪽, 북쪽, 서쪽 순서
-int n, m;
-int board1[10][10]; // 최초에 입력받은 board를 저장할 변수
-int board2[10][10]; // 사각 지대의 개수를 세기 위해 사용할 변수
-vector<pair<int,int> > cctv; // cctv의 좌표를 저장할 변수
+const int MX = 9;
+int N, M, map[MX][MX], minn;
 
-bool OOB(int a, int b){ // Out Of Bounds 확인
-  return a < 0 || a >= n || b < 0 || b >= m;
+const int dy[4] = { 0, 1, 0, -1 };         // → ↓ ← ↑
+const int dx[4] = { 1, 0, -1, 0 };
+
+const vector<vector<vector<int>>> dirCam = { 
+	// ⭐카메라의 종류별 감시 방향
+	{}, // 0번 X
+	{{0}, {1}, {2}, {3} },                 // 1번 (→ ↓ ← ↑)
+	{{0,2}, {1,3}},                        // 2번 (↔ ↕)
+	{{3,0}, {0,1}, {1,2}, {2,3} },         // 3번 (└ ┌ ┐ ┘)
+	{{2,3,0}, {3,0,1}, {0,1,2}, {1,2,3} }, // 4번 (ㅗ ㅏ ㅜ ㅓ)
+	{{0,1,2,3} }                           // 5번 (+)
+};
+
+void cctv(int y, int x, int dir, bool vis[MX][MX]) {
+	while (1) { //⭐이렇게 병합
+		y += dy[dir];
+		x += dx[dir];
+		if (y >= N || x >= M || y < 0 || x < 0 || map[y][x]==6) break;
+		vis[y][x] = 1;
+	}
 }
 
-// (x,y)에서 dir 방향으로 진행하면서 벽을 만날 때 까지 지나치는 모든 빈칸을 7로 바꿔버림
-void upd(int x, int y, int dir){
-  dir %= 4;
-  while(1){
-    x += dx[dir];
-    y += dy[dir];
-    if(OOB(x,y) || board2[x][y] == 6) return; // 범위를 벗어났거나 벽을 만나면 함수를 탈출
-    if(board2[x][y] != 0) continue; // 해당 칸이 빈칸이 아닐 경우(=cctv가 있을 경우) 넘어감
-    board2[x][y] = 7; // 빈칸을 7로 덮음
-  }
+void count(bool vis[MX][MX]) {
+	int cnt = 0;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			//⭐if (vis[i][j]) cnt++; 
+			if (map[i][j]==0 && !vis[i][j]) cnt++;
+		}
+	}
+	minn = min(minn, cnt);
 }
 
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
-  int mn = 0; // 사각 지대의 최소 크기 (=답)
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      cin >> board1[i][j];
-      if(board1[i][j] != 0 && board1[i][j] != 6)
-        cctv.push_back({i,j});
-      if(board1[i][j] == 0) mn++;
-    }
-  }
-  // 1 << (2*cctv.size())는 4의 cctv.size()승을 의미.
-  for(int tmp = 0; tmp < (1<<(2*cctv.size())); tmp++){ // tmp를 4진법으로 뒀을 때 각 자리수를 cctv의 방향으로 생각할 것이다.
-    for(int i = 0; i < n; i++)
-      for(int j = 0; j < m; j++)
-        board2[i][j] = board1[i][j];
-    int brute = tmp;    
-    for(int i = 0; i < cctv.size(); i++){
-      int dir = brute % 4;
-      brute /= 4;
-      int x = cctv[i].X;
-      int y = cctv[i].Y; // tie(x, y) = cctv[i];로 쓰면 1줄로 줄일 수 있음
-      if(board1[x][y] == 1){
-        upd(x,y,dir);
-      }
-      else if(board1[x][y] == 2){
-        upd(x,y,dir);
-        upd(x,y,dir+2);
-      }
-      else if(board1[x][y] == 3){
-        upd(x,y,dir);
-        upd(x,y,dir+1);
-      }
-      else if(board1[x][y] == 4){
-        upd(x,y,dir);
-        upd(x,y,dir+1);
-        upd(x,y,dir+2);
-      }
-      else{ // board1[x][y] == 5
-        upd(x,y,dir);
-        upd(x,y,dir+1);
-        upd(x,y,dir+2);
-        upd(x,y,dir+3);
-      }
-    }
-    int val = 0;
-    for(int i = 0; i < n; i++)
-      for(int j = 0; j < m; j++)
-        val += (board2[i][j]==0);
-    mn = min(mn, val);
-  }
-  cout << mn;
+struct Camera {
+	int c, y, x;
+	bool operator < (const Camera& other) const { //⭐
+		return c < other.c; // 54321로 내림차순 정렬
+	}
+};
+
+void dfs(int dep, bool visit[MX][MX], priority_queue<Camera> cams) { 
+	if (dep==0) {
+		count(visit);
+		return;
+	}
+
+	bool backup[MX][MX];
+	memcpy(backup, visit, sizeof(backup));
+
+	Camera cam = cams.top(); cams.pop();
+
+	for (auto& dirs : dirCam[cam.c]) { 
+	//⭐for (const auto& dirs : dirCam[cam.c]) {
+		for (int dir : dirs) cctv(cam.y, cam.x, dir, visit);
+		dfs(dep - 1, visit, cams);
+		memcpy(visit, backup, sizeof(backup));
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	
+	// 입력 받기
+	cin >> N >> M;
+	priority_queue<Camera> cams;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			cin >> map[i][j];
+			if (map[i][j] != 0 && map[i][j] != 6) {
+				cams.push({ map[i][j], i, j });
+			}
+		}
+	}
+
+	bool visit[MX][MX] = {}; //⭐
+	while (!cams.empty() && cams.top().c == 5) { 
+		Camera cam = cams.top(); cams.pop();
+		for (int dir : dirCam[5][0]) cctv(cam.y, cam.x, dir, visit);
+	}
+
+	minn = N * M;
+	dfs(cams.size(), visit, cams);
+	cout << minn;
+	return 0;
 }

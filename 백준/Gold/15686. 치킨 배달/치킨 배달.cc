8782a03fa88bd8696ff120b0549c294d5ed93870
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-05-18 14:24:09 +0900

    [Gold V] Title: 치킨 배달, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,57 +1,54 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <climits>
+#include <algorithm>
 using namespace std;
 
-struct Node {
-	int y, x;
-};
-
 int N, M, ans = INT_MAX;
-vector<Node> home, chicken;
+struct Node { int y, x; };
+vector<Node> home, chicken, selected;
 
-void dist(vector<Node>& selected) {
+int calDist(vector<Node>& selected) {
 	int sum = 0;
 	for (const auto& h : home) {
-		int minn = 2*N; 
+		int dist = 2 * N;
 		for (const auto& c : selected) {
-			minn = min(minn, abs(h.y-c.y) + abs(h.x-c.x));
+			dist = min(dist, abs(h.y - c.y) + abs(h.x - c.x));
+			if (dist == 1) break;
 		}
-		sum += minn; 
+		sum += dist;
+		if (sum >= ans) return INT_MAX;
 	}
-	ans = min(ans, sum);
+	return sum;
 }
 
-void dfs(int idx, vector<Node>& selected) { 
-	if (selected.size()==M) {
-		dist(selected);
+void dfs(int idx, vector<Node>& selected) {
+	if ((int)selected.size() >= M) {
+		ans = min(ans, calDist(selected));
 		return;
 	}
-	
-	if (idx >= chicken.size()) return;
+
+	if (idx >= (int)chicken.size()) return;
 
 	dfs(idx + 1, selected);
 	selected.push_back(chicken[idx]);
 	dfs(idx + 1, selected);
-	selected.pop_back(); 
+	selected.pop_back();
 }
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
-
+	
 	cin >> N >> M;
-    
 	int tmp;
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < N; ++j) {
 			cin >> tmp;
-			if (tmp == 1) home.push_back({ i, j });
+			if (tmp == 1) home.push_back({i, j});
 			else if (tmp == 2) chicken.push_back({ i, j });
 		}
 	}
 
-	vector<Node> selected = {};
 	dfs(0, selected);
 	cout << ans;
 	return 0;

af8eda2b1b00e8d60685dab2bd8d072fef83de42
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-05-18 18:35:16 +0900

    [Gold III] Title: 감시, ⭐벡터, Time: 4 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,14 +1,17 @@
+//[Gold III] Title: 감시, ⭐벡터, Time: 4 ms, Memory: 2216 KB -BaekjoonHub
 #include <iostream>
+#include <queue> 
 #include <vector>
 #include <cstring>
-#include <algorithm>
 using namespace std;
 
 const int MX = 9;
 int N, M, map[MX][MX], ans;
-const int dy[4] = { 0, 1, 0, -1 }, dx[4] = { 1, 0, -1, 0 };
 
-struct Camera { int c, y, x; };
+const int dy[4] = { 0, 1, 0, -1 };
+const int dx[4] = { 1, 0, -1, 0 };
+
+struct Camera { int c, y, x; }; //⭐
 
 const vector<vector<vector<int>>> dirCam = {
 	{},
@@ -23,7 +26,7 @@ void cctv(int y, int x, int dir, bool vis[MX][MX]) {
 	while (1) {
 		y += dy[dir];
 		x += dx[dir];
-		if (y >= N || x >= M || y < 0 || x < 0 || map[y][x] == 6) break;
+		if (y >= N || x >= M || y < 0 || x < 0 || map[y][x]==6) break;
 		vis[y][x] = 1;
 	}
 }
@@ -32,53 +35,56 @@ void count(bool vis[MX][MX]) {
 	int cnt = 0;
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < M; ++j) {
-			if (map[i][j] == 0 && !vis[i][j]) cnt++;
+			if (map[i][j]==0 && !vis[i][j]) cnt++;
 		}
 	}
 	ans = min(ans, cnt);
 }
 
-void dfs(int dep, bool vis[MX][MX], const vector<Camera>& cams) {
-	if (dep == cams.size()) {
-		count(vis);
+void dfs(int dep, bool visit[MX][MX], const vector<Camera>& cams) {  //⭐const vector &
+	if (dep==cams.size()) { //⭐
+		count(visit);
 		return;
 	}
 
 	bool backup[MX][MX];
-	memcpy(backup, vis, sizeof(backup));
+	memcpy(backup, visit, sizeof(backup));
 
-	const Camera& cam = cams[dep];
+	const Camera& cam = cams[dep]; //⭐
 	for (const auto& dirs : dirCam[cam.c]) {
-		for (int d : dirs) cctv(cam.y, cam.x, d, vis);
-		dfs(dep + 1, vis, cams);
-		memcpy(vis, backup, sizeof(backup));
+		for (int d : dirs) cctv(cam.y, cam.x, d, visit);
+		dfs(dep + 1, visit, cams); //
+		memcpy(visit, backup, sizeof(backup)); //⭐
 	}
 }
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
+	freopen_s(new FILE*, "input.txt", "r", stdin);
+	
+	cin >> N >> M;
+	ans = N * M; 
 
-	cin >> N >> M; ans = N * M;
-
-	vector<Camera> cams, cam5;
-	bool visited[MX][MX] = {};
+	vector<Camera> cams, cam5; //
+	bool visit[MX][MX] = {}; 
 
 	int cam;
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < M; ++j) {
 			cin >> cam; map[i][j] = cam;
 			if (cam == 0 || cam == 6) continue;
-			if (cam == 5) cam5.push_back({ cam, i, j });
-			else cams.push_back({ cam, i, j });
+			if (cam == 5) cam5.push_back({ cam, i, j }); // 5일 때
+			else cams.push_back({ cam, i, j }); 
 		}
 	}
 
+	// 캠이 5일 때 먼저 처리
 	while (!cam5.empty()) {
 		Camera cam = cam5.back(); cam5.pop_back();
-		for (int dir : dirCam[5][0]) cctv(cam.y, cam.x, dir, visited);
+		for (int dir : dirCam[5][0]) cctv(cam.y, cam.x, dir, visit);
 	}
 
-	dfs(0, visited, cams);
+	dfs(0, visit, cams); //
 	cout << ans;
 	return 0;
 }

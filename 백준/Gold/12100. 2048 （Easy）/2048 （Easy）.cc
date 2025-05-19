ba38c2bc479f3a16b514ca28bd276adad74511f8
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-05-19 15:13:38 +0900

    [Gold I] Title: 2048 (Easy), Time: 4 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.cc
@@ -1,62 +1,137 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+#include <cstring>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
-int board1[21][21];
-int board2[21][21];
-int n;
+const int MX = 21;
+int N, map[MX][MX], ans=INT_MIN;
 
-void rotate(){ // board2를 시계 방향으로 90도 회전하는 함수
-  int tmp[21][21];
-  for(int i = 0; i < n; i++)
-    for(int j = 0; j < n; j++)
-      tmp[i][j] = board2[i][j];
-  for(int i = 0; i < n; i++)
-    for(int j = 0; j < n; j++)
-      board2[i][j] = tmp[n-1-j][i];
+void up(int map[MX][MX]) {
+	queue<int> q;
+	for (int x = 0; x < N; ++x) {
+		for (int y = 0; y < N; ++y) {
+			if (map[y][x] != 0) q.push(map[y][x]);
+			map[y][x] = 0;
+		}
+		int idx = 0, pre = 0, nxt = 0;
+		while (!q.empty()) {
+			if (pre == 0) { pre = q.front(); q.pop(); }
+			else { 
+				nxt = q.front(); q.pop();
+				map[idx][x] = pre;
+				if (pre == nxt) { pre = 0; map[idx][x] += nxt; }
+				else pre = nxt; 
+				idx++;
+			}
+		}
+		if (pre != 0) map[idx][x] = pre;
+	}
 }
 
-void tilt(int dir){
-  while(dir--) rotate();
-  for(int i = 0; i < n; i++){
-    int tilted[21] = {}; // board2[i]를 왼쪽으로 기울인 결과를 저장할 변수
-    int idx = 0; // tilted 배열에서 어디에 삽입해야 하는지 가리키는 변수
-    for(int j = 0; j < n; j++){
-      if(board2[i][j] == 0) continue;
-      if(tilted[idx] == 0) // 삽입할 위치가 비어있을 경우
-        tilted[idx] = board2[i][j];
-      else if(tilted[idx] == board2[i][j]) // 같은 값을 갖는 블록이 충돌할 경우
-        tilted[idx++] *= 2;
-      else // 다른 값을 갖는 블록이 충돌
-        tilted[++idx] = board2[i][j];
-    }
-    for(int j = 0; j < n; j++) board2[i][j] = tilted[j]; // board2[i]에 tilted를 덮어씀
-  }
+void right(int map[MX][MX]) {
+	queue<int> q;
+	for (int y = 0; y < N; ++y) {	
+		for (int x = N - 1; x >= 0; --x) {
+			if (map[y][x] != 0) q.push(map[y][x]);
+			map[y][x] = 0;
+		}
+		int idx = N - 1, pre = 0, nxt = 0;
+		while (!q.empty()) {
+			if (pre == 0) { pre = q.front(); q.pop(); }
+			else {
+				nxt = q.front(); q.pop();
+				map[y][idx] = pre;
+				if (pre == nxt) { pre = 0; map[y][idx] += nxt; }
+				else pre = nxt;
+				idx--;
+			}
+		}
+		if (pre != 0) map[y][idx] = pre;
+	}
 }
 
+void down(int map[MX][MX]) {
+	queue<int> q;
+	for (int x = 0; x < N; ++x) {
+		for (int y = N - 1; y >= 0; --y) {
+			if (map[y][x] != 0) q.push(map[y][x]);
+			map[y][x] = 0;
+		}
+		int idx = N - 1, pre = 0, nxt = 0;
+		while (!q.empty()) {
+			if (pre == 0) { pre = q.front(); q.pop(); }
+			else {
+				nxt = q.front(); q.pop();
+				map[idx][x] = pre;
+				if (pre == nxt) { pre = 0; map[idx][x] += nxt; }
+				else pre = nxt;
+				idx--;
+			}
+		}
+		if (pre != 0) map[idx][x] = pre;
+	}
+}
 
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+void left(int map[MX][MX]) {
+	queue<int> q;
+	for (int y = 0; y < N; ++y) {
+		for (int x = 0; x < N; ++x) {
+			if (map[y][x] != 0) q.push(map[y][x]);
+			map[y][x] = 0;
+		}
+		int idx = 0, pre = 0, nxt = 0;
+		while (!q.empty()) {
+			if (pre == 0) { pre = q.front(); q.pop(); }
+			else {
+				nxt = q.front(); q.pop();
+				map[y][idx] = pre;
+				if (pre == nxt) { pre = 0; map[y][idx] += nxt; }
+				else pre = nxt;
+				idx++;
+			}
+		}
+		if (pre != 0) map[y][idx] = pre;
+	}
+}
 
-  cin >> n;
-  for(int i = 0; i < n; i++)
-    for(int j = 0; j < n; j++)
-      cin >> board1[i][j];
+void comb(int dir, int map[MX][MX]) {
+	if (dir == 0) up(map);
+	else if (dir == 1) right(map);
+	else if (dir == 2) down(map);
+	else left(map);
+}
 
-  int mx = 0;
-  for(int tmp = 0; tmp < 1024; tmp++){
-    for(int i = 0; i < n; i++)
-      for(int j = 0; j < n; j++)
-        board2[i][j] = board1[i][j];
-    int brute = tmp;
-    for(int i = 0; i < 5; i++){
-      int dir = brute % 4;
-      brute /= 4;
-      tilt(dir);
-    }
-    for(int i = 0; i < n; i++)
-      for(int j = 0; j < n; j++)
-        mx = max(mx, board2[i][j]);
-  }
-  cout << mx;
+void dfs(int dep, int map[MX][MX]) {
+	if (dep >=5) {
+		for (int i = 0; i < N; ++i) {
+			ans = max(ans, *max_element(map[i], map[i] + N));
+		}
+		return;
+	}
+
+	int backup[MX][MX];
+	memcpy(backup, map, sizeof(backup));
+
+	for (int dir = 0; dir < 4; ++dir) {
+		comb(dir, map);
+		dfs(dep + 1, map);
+		memcpy(map, backup, sizeof(backup));
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			cin >> map[i][j];
+		}
+	}
+
+	dfs(0, map);
+	cout << ans;
+	return 0;
 }

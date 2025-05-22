546af4a9f3d800246a7a09852f21af7632b9bc01
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-05-22 19:57:52 +0900

    [docs] BOJ 15683 감시 바킹독 해석

--- 백준/Gold/15683. 감시/bark_감시.cpp
+++ 백준/Gold/15683. 감시/bark_감시.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int n, m;
 
-int dy[4] = {0,1,0,-1}; // 남쪽, 동쪽, 북쪽, 서쪽 순서로 / 북쪽을 시작으로 시계방향
+int dy[4] = {0,1,0,-1};
 int dx[4] = {1,0,-1,0};
 
-int board1[10][10]; // 입력으로 주어지는 사무실의 모양 저장
+int board1[10][10]; // 입력으로 주어지는 초기 상태
 int board2[10][10]; // cctv의 방향을 정한 후에 사각지대를 세기 위한 변수
 
 vector<pair<int,int> > cctv; // cctv의 좌표를 저장할 변수
@@ -18,7 +18,7 @@ bool OOB(int a, int b){ // 영역을 벗어나는지 확인하는 함수
   return a < 0 || a >= n || b < 0 || b >= m;
 }
 
-// (x,y)에서 dir 방향으로 진행하면서 벽을 만나기 전까지의 모든 빈칸을 7로 바꿈
+// (x,y)에서 dir 방향으로 진행하면서 벽을 만나기 전까지의 모든 빈칸0을 7로 바꿈
 void upd(int x, int y, int dir){
   dir %= 4;
   while(1){
@@ -34,7 +34,9 @@ int main(void) {
   ios::sync_with_stdio(0); cin.tie(0);
     
   cin >> n >> m;
-  // cctv가 1개 이상이라는 말이 없음 = 0개일 수도 있음 -> ans을 0x7f7f7f7f같은 값으로 두는 것보다 안전함
+  
+  //⭐cctv가 1개 이상이라는 말이 없음 = 0개일 수도 있음 
+  // 이 때문에 ans을 0x7f7f7f7f같은 값으로 두는 것보다 직접 카운트하는 것이 안전함
   int ans = 0; // 사각 지대의 최소 크기 = 답
   for(int i = 0; i < n; i++){
     for(int j = 0; j < m; j++){
@@ -59,11 +61,11 @@ int main(void) {
       
     int brute = tmp;    
     for(int i = 0; i < cctv.size(); i++){ // 카메라가 3대였을 떈, 0 ~ 3
-      int dir = brute % 4; // 1의 자리수 = 각 자리수가 방향
-      brute /= 4;          // 다음 자리수 / tmp의 값을 4진법으로 바꾸기 위해 나눔
+      int dir = brute % 4; // 1의 자리수 | 각 자리수가 각 카메라의 방향
+      brute /= 4;          // 다음 자리수 | 다음 카메라의 방향을 알기 위해 = tmp의 값을 4진법으로 바꾸기 위해 나눔
       
       int y = cctv[i].Y; // cctv 좌표 담기
-      int x = cctv[i].X; // tie(x, y) = cctv[i];로 쓰면 1줄로 줄일 수 있음
+      int x = cctv[i].X; //⭐tie(x, y) = cctv[i];로 쓰면 1줄로 줄일 수 있음
       
       if(board1[x][y] == 1){
         upd(x,y,dir);

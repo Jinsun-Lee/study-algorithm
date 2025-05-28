5c02d98bcc3830ee4087c698aa191adcb14378ca
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-05-28 21:42:06 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M, ans[8], num[8];
+
+void dfs(int depth, int start) {
+	if (depth >= M) {
+		for (int i = 0; i < M; ++i) cout << ans[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = start; i < N; ++i) {
+		ans[depth] = num[i];
+		dfs(depth + 1, i+1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i) cin >> num[i];
+	sort(&num[0], &num[N]);
+    dfs(0, 0);
+	return 0;
+}

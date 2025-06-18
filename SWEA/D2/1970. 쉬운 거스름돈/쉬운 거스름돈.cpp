d45455b23200ab2a16377f05fc88b70b4f18b53e
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-06-18 14:24:51 +0900

    [D2] Title: 쉬운 거스름돈, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.cpp
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-
-const int MX = 8;
-// 5e4, 1e4, 5e3, ..., 1e1은 더블, 웹에서 컴파일 에러, (int)5e4는 길어서
-int unit[MX] = { 50000, 10000, 5000, 1000, 500, 100, 50, 10 };
-int N, ans[MX]; // 거슬러 주어야 할 금액, 각 종류의 돈의 수
-
-void dfs(int depth, int res) {
-	if (res == 0 || depth >= MX) return;
-
-	int carry = res / unit[depth];
-	ans[depth] = carry;
-	res -= carry * unit[depth];
-	dfs(depth + 1, res);
-}
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 
-	int tc; cin >> tc;
-	for (int t = 1; t <= tc; ++t) {
-		fill(ans, ans + MX, 0); // 초기화
-		cin >> N;
+	const int unit[8] = { 50000, 10000, 5000, 1000, 500, 100, 50, 10 };
 
-		dfs(0, N); // 현재단위, 남은금액
-		cout << "#" << t << "\n";
+	int tCase; cin >> tCase;
+	for (int tc = 1; tc <= tCase; ++tc) {
+		
+		int N; cin >> N;
+		int ans[8] = {}; // 0으로 초기화
+
+		for (int i = 0; i < 8; ++i) { // unit.size() 못 써
+			if (N == 0) break;
+			if (N < unit[i]) continue;
+
+			ans[i] = N / unit[i];
+			N = N - (unit[i] * ans[i]);
+		}
+
+		cout << "#" << tc << "\n";
 		for (int i : ans) cout << i << " ";
 		cout << "\n";
 	}

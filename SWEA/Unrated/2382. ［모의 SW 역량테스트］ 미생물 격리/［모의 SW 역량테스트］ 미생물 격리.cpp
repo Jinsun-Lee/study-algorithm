e5884f28b3ef499781567ed84441297c44bc55fe
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-06-23 14:04:07 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 146 ms, Memory: 6,120 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.cpp
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.cpp
@@ -1,48 +1,53 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
 using namespace std;
 
-const int dy[4] = { -1,1,0,0 }, dx[4] = { 0,0,-1,1 }; // 상하좌우
-const int newDir[4] = { 1, 0, 3, 2 }; // 새로운 방향, 함수 대체
+const int dy[4] = { -1,1,0,0 }, dx[4] = { 0,0,-1,1 };
+const int newDir[4] = { 1, 0, 3, 2 }; // 상하좌우 -> 반대방향
 
-int N, M, K; // 격자크기, 반복시간, 미생물군집수
+int N, M, K;
 
 struct Cell { int y, x, cnt, dir; };
 vector<Cell> cells; // y, x, 군집수, 방향
+vector<pair<int, int>> visited;
 
-const int MX = 100;
+const int MX = 101;
 int sumCnt[MX][MX], maxCnt[MX][MX], maxDir[MX][MX];
 
 void input() {
 	cin >> N >> M >> K;
 
-	cells.clear(); // 초기화
+	cells.clear(); // 주의
 	cells.reserve(K); // 재할당 최소화
+	visited.reserve(K); 
 
 	int y, x, cnt, dir;
 	while (K--) {
 		cin >> y >> x >> cnt >> dir;
-		cells.push_back({y, x, cnt, dir-1});
+		cells.push_back({ y, x, cnt, dir - 1 });
 	}
 }
 
 void move() {
-	// 초기화
-	memset(sumCnt, 0, sizeof(sumCnt));
-	memset(maxCnt, 0, sizeof(maxCnt));
-	memset(maxDir, 0, sizeof(maxDir));
+	// 초기화 memset보다 효율
+	visited.clear();
 
-	// 일단 전부 이동을 해
+	// 일단 전부 이동
 	for (auto& cur : cells) {
 		int ny = cur.y + dy[cur.dir];
 		int nx = cur.x + dx[cur.dir];
 
-		// 이동 후 빨간 테두리면 절반(0이면 추가X), 이동 반대 
+		// 이동 후 빨간 테두리면 절반(0이면 추가X), 반대 
 		if (ny == N - 1 || nx == N - 1 || ny == 0 || nx == 0) {
 			cur.cnt >>= 1; // 비트 시프트로 나눗셈 대체 cur.cnt /= 2;
-			if (cur.cnt == 0) continue; 
+			if (cur.cnt == 0) continue;
 			cur.dir = newDir[cur.dir];
+		}
+
+		// 접근한 것만 기록 + 초기 접근 시 초기화
+		if (sumCnt[ny][nx] == 0) {
+			visited.emplace_back(ny, nx);
+			maxCnt[ny][nx] = 0; // 별도로 필요
 		}
 
 		sumCnt[ny][nx] += cur.cnt;
@@ -54,18 +59,18 @@ void move() {
 
 	// 합치기 동작
 	cells.clear();
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-			if (sumCnt[i][j]==0) continue;
-			cells.push_back({i, j, sumCnt[i][j], maxDir[i][j]});
-		}
+	for (auto& p : visited) {
+		int y = p.first, x = p.second;
+		cells.push_back({ y, x, sumCnt[y][x], maxDir[y][x] });
+
+		// 접근한 위치만 초기화
+		sumCnt[y][x] = maxCnt[y][x] = maxDir[y][x] = 0;
 	}
 }
 
 int cntCells() {
-	// cur.cnt를 continue 뒤에서 갱신해도 continue라 어차피 sumCnt에 안 더해서 괜찮아 cells.clear()하고 갱신하니까
 	int sum = 0;
-	for (const Cell& c : cells) sum += c.cnt; // 이중for문 X
+	for (const auto& c : cells) sum += c.cnt;
 	return sum;
 }
 

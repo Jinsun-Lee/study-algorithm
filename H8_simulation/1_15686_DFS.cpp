788e42f01e17fcbe6629b959240009900727e244
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-09-03 16:57:20 +0900

    [solve] BOJ 15686 치킨배달

--- H8_simulation/1_15686_DFS.cpp
+++ H8_simulation/1_15686_DFS.cpp
@@ -8,19 +8,21 @@ int N, M, ans = 0x7fffffff;
 struct Node { int y, x; };
 vector<Node> home, chicken, selected;
 
-void dist(vector<Node>& selected) {
+void calDist(vector<Node>& selected) {
 	int sum = 0;
     
 	for (const auto& h : home) {
-		int minn = 2*N; 
+		int minDist = 2 * N;
         
 		for (const auto& c : selected) {
-			minn = min(minn, abs(h.y-c.y) + abs(h.x-c.x));
-            if (minn == 1) break;
+			int dist = abs(h.y-c.y) + abs(h.x-c.x);
+			minDist = min(minDist, dist);
+
+			if (minDist == 1) break; // 최소거리 나오면 탐색 불필요
 		}
         
-		sum += minn; 
-        if (sum>=ans) return;
+		sum += minDist; 
+        if (sum>=ans) return; // 현재까지의 거리가 ans보다 크면 탐색 불필요
 	}
     
 	ans = min(ans, sum);
@@ -28,16 +30,16 @@ void dist(vector<Node>& selected) {
 
 void DFS(int idx, vector<Node>& selected) { 
 	if (selected.size()==M) {
-		dist(selected);
+		calDist(selected);
 		return;
 	}
 	
-	if (idx == chicken.size()) return;
-    if (selected.size() + (chicken.size() - idx) < M) return;
+	if (idx == chicken.size()) return; // 치킨집 다 보면 탐색 종료
+    if (selected.size() + (chicken.size() - idx) < M) return; // 남은 치킨집의 수가 부족할 경우
     
-	DFS(idx + 1, selected);
+	DFS(idx + 1, selected); // 선택 안 할 경우
 	selected.push_back(chicken[idx]);
-	DFS(idx + 1, selected);
+	DFS(idx + 1, selected); // 선택 할 경우
 	selected.pop_back(); 
 }
 
@@ -54,6 +56,7 @@ int main() {
 		}
 	}
 
+	//vector<Node> selected = {}; // 전역으로 해야 0ms 나와
 	DFS(0, selected);
     
 	cout << ans;

b291f416df429798d03d32ef8b73ab2ef9f56b47
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2025-09-09 17:12:13 +0900

    [solve] BOJ 2573 빙산, Memory: 3000 KB, Time: 50 ms, Code: 2042 B

--- D4_BFS/2573_won.cpp
+++ D4_BFS/2573_won.cpp
@@ -14,19 +14,17 @@ int N, M, map[MX][MX], tmp[MX][MX];
 bool vis[MX][MX];
 
 struct Node { int y, x; };
-vector<Node> ice;
 
 bool isEmpty = false;
 
-void solve() {
+void solve() { // 빙하 녹이기
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < M; ++j) {
-            
-			if (map[i][j]) { // 얼음이 없는 것 패스
+
+			if (map[i][j]) { // 얼음이 있어야 녹이지
 				for (int d = 0; d < 4; ++d) {
 					int ny = i + dy[d];
 					int nx = j + dx[d];
-
 					if (ny >= N || nx >= M || ny < 0 || nx < 0) continue;
 					if (map[ny][nx] == 0) ++tmp[i][j]; // 인접한 칸이 0이 되면 나중에 적용하게
 				}
@@ -34,24 +32,21 @@ void solve() {
 		}
 	}
 
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) map[i][j] = max(0, map[i][j] - tmp[i][j]);
+	for (int i = 0; i < N; ++i) { // 따로 반영
+		for (int j = 0; j < M; ++j) map[i][j] = max(0, map[i][j]-tmp[i][j]);
 	}
 }
 
-void BFS(int y, int x) {
+void BFS(int y, int x) { 
 	vis[y][x] = true;
 
-	queue<pair<int, int>> q;
+	queue<Node> q;
 	q.push({ y, x });
-
 	while (!q.empty()) {
-		auto cur = q.front(); q.pop();
-
-		for (int i = 0; i < 4; ++i) {
-			int ny = cur.first + dy[i];
-			int nx = cur.second + dx[i];
-
+		auto c = q.front(); q.pop();
+		for (int d = 0; d < 4; ++d) {
+			int ny = c.y + dy[d];
+			int nx = c.x + dx[d];
 			if (ny >= N || nx >= M || ny < 0 || nx < 0) continue;
 			if (vis[ny][nx] || map[ny][nx] == 0) continue;
 			vis[ny][nx] = true;
@@ -66,8 +61,7 @@ bool check() { // 연결 요소 개수 확인
 	int connected = 0;
 	for (int i = 0; i < N; ++i) {
 		for (int j = 0; j < M; ++j) {
-
-			if (map[i][j] && vis[i][j] == false) {
+			if (map[i][j]>0 && vis[i][j]==false) { // 얼음이 있어야 녹이지
 				BFS(i, j);
 				++connected;
 			}
@@ -75,7 +69,7 @@ bool check() { // 연결 요소 개수 확인
 	}
 
 	if (connected > 1) return true; // 분리
-	else if (connected == 0) { // 분리되지 않은 채로 전부 삭제
+	else if (connected == 0) { // 빙산이 전부 다 녹아서 없어 = 얼음이 하나도 없어
 		isEmpty = true;
 		return true;
 	}
@@ -83,6 +77,7 @@ bool check() { // 연결 요소 개수 확인
 }
 
 int main() {
+	freopen_s(new FILE*, "input.txt", "r", stdin);
 	ios::sync_with_stdio(0); cin.tie(0);
 	cin >> N >> M;
 
@@ -91,16 +86,15 @@ int main() {
 	}
 
 	int ans = 0;
-	while(1) {
+	while (1) {
 		memset(tmp, 0, sizeof(tmp));
 
-		solve(); // 빙산 녹이기
-		++ans;
+		solve(); ++ans; // 빙산 녹이기
 
 		if (check()) break;
-	} 
+	}
 
-	if (isEmpty) cout << 0;
+	if (isEmpty) cout << 0; // 주의
 	else cout << ans;
 	return 0;
 }
